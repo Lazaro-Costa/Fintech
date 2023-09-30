54bbbed3c8d4e224070a36ffc0f300eebe217117
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-09-30 12:24:44 -0300

    Criado o CSS Base

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { DataContextProvider } from "./Context/DataContext"
 function App() {
   return (
     <DataContextProvider>
-    <div>
+    <div className="container">
       <Sidenav/>
       <main>
       <Header/>
