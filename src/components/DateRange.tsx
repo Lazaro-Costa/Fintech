6941f11b3127d3259d21974f88d2ab07c53358dd
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-09-30 13:09:45 -0300

    Criado sitema de Data Reativa

--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import DataInput from './DateInput'
+import { useData } from '../Context/DataContext'
 
 const DateRange = () => {
-  const [inicio, setInicio] = React.useState('')
-  const [final, setFinal] = React.useState('')
+  const {inicio, setInicio, final, setFinal} = useData()
   return (
     <form onSubmit={(e) => e.preventDefault()} className='box flex'>
       <DataInput label="Início" value={inicio} onChange={({target}) => setInicio(target.value)}/>
+      {inicio}
       <DataInput label="Final" value={final} onChange={({target}) => setFinal(target.value)}/>
+      {final}
     </form>
   )
 }
