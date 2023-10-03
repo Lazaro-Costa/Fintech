7a9416e90519d90bdc64aa890070a4da4636910f
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-10-03 09:18:55 -0300

    Criado o menu de meses e correção de bugs

--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -7,9 +7,7 @@ const DateRange = () => {
   return (
     <form onSubmit={(e) => e.preventDefault()} className='box flex'>
       <DataInput label="Início" value={inicio} onChange={({target}) => setInicio(target.value)}/>
-      {inicio}
-      <DataInput label="Final" value={final} onChange={({target}) => setFinal(target.value)}/>
-      {final}
+      <DataInput label="Final" value={final} onChange={({target}) => setFinal(target.value)}/>      
     </form>
   )
 }
