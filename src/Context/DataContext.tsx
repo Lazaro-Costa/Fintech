8bb722ba1474af2047beb7567bb1cac5b740668f
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-10-05 09:01:45 -0300

    Feat: Visualizar Venda unica

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import useFetch from '../Hooks/useFetch';
-import { func } from 'prop-types';
 
 export type IVenda ={
   id: string;
--- src/pages/Venda.tsx
+++ src/pages/Venda.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../Hooks/useFetch';
+import { IVenda } from '../Context/DataContext';
+import Loading from '../components/Loading';
+
+type VendaSemData = Omit<IVenda, 'data'>
+
 
 const Venda = () => {
+  const {id} = useParams();
+  const {data, loading} = useFetch<VendaSemData>(`https://data.origamid.dev/vendas/${id}`);
+  if(loading === true) return <Loading/>
+  if(data === null) return null;
   return (
-    <div>
-      Venda
+    <div key={data.id} className='vendaInfo'>
+      <div className='box mb'><p>ID: <span>{data.id}</span></p></div>
+      <div className='box mb'><p>Nome: <span>{data.nome}</span></p></div>
+      <div className='box mb'><p>Preço: <span>
+        {data.preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
+        </span></p></div>
+      <div className='box mb'><p>Status: <span>{data.status}</span></p></div>
+      <div className='box mb'><p>Pagamento: <span>{data.pagamento}</span></p></div>
     </div>
   )
 }
